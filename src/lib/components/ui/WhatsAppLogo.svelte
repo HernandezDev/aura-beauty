2a37adbd1edc679468bb9fdcdab2976d345548ea
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2026-01-25 23:11:54 -0300

    Refactor WhatsAppLogo component: update SVG structure and remove unused path

--- src/lib/components/ui/WhatsAppLogo.svelte
+++ src/lib/components/ui/WhatsAppLogo.svelte
@@ -7,9 +7,9 @@
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
   width="24"
   height="24"
-  viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
   stroke-width="2"
@@ -18,12 +18,15 @@
   class={cn("select-none fill-current", className)}
   {...rest}
 >
+  <!-- Message bubble -->
   <path
     d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"
   />
-  <path
-    d="M15.5 13.5a.6.6 0 0 0 .7-.2l.2-.3a1.2 1.2 0 0 1 1.1-.5h1.8a1.2 1.2 0 0 1 1.2 1.2v1.8a1.2 1.2 0 0 1-1.2 1.2 10.8 10.8 0 0 1-10.8-10.8 1.2 1.2 0 0 1 1.2-1.2h1.8a1.2 1.2 0 0 1 1.2 1.2l-.1.2a.6.6 0 0 0 .2.7l3.8 3.8z"
-    transform="translate(-2, -2) scale(1.1)"
-    transform-origin="center"
-  />
+
+  <!-- Phone (centered inside bubble) -->
+  <g transform="translate(5.6 5.8) scale(0.52)">
+    <path
+      d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"
+    />
+  </g>
 </svg>
