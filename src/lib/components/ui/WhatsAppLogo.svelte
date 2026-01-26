b3632ec3cf7d14cda33e8b54baedcfaa5875713d
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2026-01-25 23:51:15 -0300

    Update WhatsAppLogo component: change SVG dimensions to em units for better scalability

--- src/lib/components/ui/WhatsAppLogo.svelte
+++ src/lib/components/ui/WhatsAppLogo.svelte
@@ -8,8 +8,8 @@
 <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
-  width="24"
-  height="24"
+  width="1.2em"
+  height="1.2em"
   fill="none"
   stroke="currentColor"
   stroke-width="2"
