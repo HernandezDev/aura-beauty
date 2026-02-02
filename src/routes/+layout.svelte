6d94fd74921de149b291e60c5655ce7e8d1aedc2
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2026-02-02 15:32:02 -0300

    refactor: preload font assets in layout component for improved performance

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
   import "$lib/styles/layout.css";
   import favicon from "$lib/assets/favicon.svg";
+  import fontSans from "@fontsource-variable/manrope/files/manrope-latin-wght-normal.woff2?url";
+  import fontSerif from "@fontsource-variable/lora/files/lora-latin-wght-normal.woff2?url";
   let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link
+    rel="preload"
+    href={fontSans}
+    as="font"
+    type="font/woff2"
+    crossorigin="anonymous"
+  />
+  <link
+    rel="preload"
+    href={fontSerif}
+    as="font"
+    type="font/woff2"
+    crossorigin="anonymous"
+  />
+  <link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
